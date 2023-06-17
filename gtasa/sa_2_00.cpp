a89ade5af21a55dd9c247a1864fc9298206297c8
RusJJ <mctesting1@yandex.ru>
2023-06-17 15:41:57 +0300

    Update sa_2_00.cpp

--- gtasa/sa_2_00.cpp
+++ gtasa/sa_2_00.cpp
@@ -93,6 +93,43 @@ void PatchPickUpsPools()
     aml->Redirect(pGameAddr + 0x31CF98 + 0x1, (uintptr_t)PickUpsInit_Patch);
 }
 
+// Matrices
+void* (*InitMatrixLinkList)(uintptr_t, int);
+DECL_HOOKv(InitMatrixArray)
+{
+    InitMatrixLinkList(aml->GetSym(hGameHndl, "gMatrixList"), cfg->GetInt("MatrixCount", 900, "Matrices"));
+}
+
+// Scripts
+char* ScriptSearchLights;
+int PatchSearchLightsCount;
+uintptr_t SearchLights_BackTo1, SearchLights_BackTo2;
+extern "C" uintptr_t SearchLights_Inject(int val)
+{
+    if(val == PatchSearchLightsCount) return SearchLights_BackTo1;
+    return SearchLights_BackTo2;
+}
+__attribute__((optnone)) __attribute__((naked)) void SearchLights_Patch(void)
+{
+    // Original
+    asm("ADDS R4, #1");
+
+    asm("MOV R0, R8");
+    asm("BL SearchLights_Inject");
+
+    asm("BX R0");
+}
+void PatchScripts()
+{
+    int SearchLightsCount = cfg->GetInt("SearchLights", 8, "Scripts"); PatchSearchLightsCount = 992 * SearchLightsCount;
+    ScriptSearchLights = new char[SearchLightsCount * 0x7C] {0};
+    char** ScriptSearchLights_ptr = &ScriptSearchLights;
+    aml->Write(pGameAddr + 0x6797B4, (uintptr_t)ScriptSearchLights_ptr, sizeof(void*));
+    SearchLights_BackTo1 = pGameAddr + 0x358908 + 0x1;
+    SearchLights_BackTo2 = pGameAddr + 0x358812 + 0x1;
+    aml->Redirect(pGameAddr + 0x358900 + 0x1, (uintptr_t)SearchLights_Patch);
+}
+
 // CLASS DESCRIPTION
 class GTASA_2_00 : public GTASA
 {
@@ -110,4 +147,11 @@ void GTASA_2_00::GameLoaded()
     PatchPools();
     //PatchPickUpsPools();
     HOOKPLT(PoolsInit, pGameAddr + 0x672468);
+
+    // Matrices
+    HOOK(InitMatrixArray, aml->GetSym(hGameHndl, "_ZN10CPlaceable15InitMatrixArrayEv"));
+    SET_TO(InitMatrixLinkList, aml->GetSym(hGameHndl, "_ZN15CMatrixLinkList4InitEi"));
+
+    // Scripts
+    PatchScripts();
 }
